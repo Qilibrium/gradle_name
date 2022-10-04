b5600d6953adf0c73c070bb28d6897883c2c689d
Oleksandr Fomin <qilibriumultra@gmail.com>
2022-10-04 23:16:37 +0300

    Initial commit

---
+++ src/main/java/Name.java
@@ -0,0 +1,13 @@
+import com.google.gson.GsonBuilder;
+
+public class Name {
+    public static void main(String[] args) {
+
+        Person person = new Person("Oleksandr", "Fomin");
+
+        Object gson = new GsonBuilder().create().toJson(person);
+
+        System.out.println(gson);
+
+    }
+}
